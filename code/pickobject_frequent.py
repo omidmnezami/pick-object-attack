678ddffa3ecda15f684cbb9cafb536f5860a5cb2
akshay107 <thegphenomenon@gmail.com>
2021-07-28 19:37:30 +0530

    Update pickobject_frequent.py
    Made necessary changes. Look at the comments starting with "# TODO:"

--- code/pickobject_frequent.py
+++ code/pickobject_frequent.py
@@ -12,12 +12,12 @@ import numpy as np
 import time
 import sys
 from collections import Counter
-
+# TODO: Since this code is for both targeted and non-targeted. We don't need pickobject_frequent_nontargeted.py. 
 #caffe.set_mode_gpu()
 #caffe.set_device(0)
 
 # change the following path as your local path
-cfg_from_file('experiments/cfgs/faster_rcnn_end2end_resnet.yml')
+cfg_from_file('bottom-up-attention/experiments/cfgs/faster_rcnn_end2end_resnet.yml')
 
 if __name__ == '__main__':
     t0 = time.time()
@@ -39,9 +39,9 @@ if __name__ == '__main__':
     #caffe.set_multiprocess(True)
 
     ## model and prototxt
-    weights = 'demo/resnet101_faster_rcnn_final.caffemodel'
-    prototxt_test = 'demo/test.prototxt'
-    prototxt_train = 'demo/test_gradient.prototxt'
+    weights = 'bottom-up-attention/demo/resnet101_faster_rcnn_final.caffemodel'
+    prototxt_test = 'bottom-up-attention/demo/test.prototxt'   # TODO: This line is not needed. prototxt_test is not used anywhere
+    prototxt_train = 'bottom-up-attention/demo/test_gradient.prototxt'
 
     ## specifiy min and max changes
     eps = 200
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 
     MEANS = np.array([[102.9801, 115.9465, 122.7717]])
 
-    data_path = '../bottom-up-attention/data/genome/1600-400-20'
+    data_path = 'bottom-up-attention/data/genome/1600-400-20' # TODO: Change data_path in other files too
     classes = ['__nothing__']
     with open(os.path.join(data_path, 'objects_vocab.txt')) as f:
         for object in f.readlines():
@@ -281,7 +281,10 @@ if __name__ == '__main__':
          
         adversarial_x = np.clip(adversarial_x, 0.0, 255.0)
         print 'loss====== ', net.blobs['loss_cls'].data
-        print 'expected loss==', np.mean(-1*np.log(net.blobs['cls_prob'].data[:, target_idx]))
+	if targeted:
+             print 'expected loss==', np.mean(-1*np.log(net.blobs['cls_prob'].data[:, new_class]))
+	else:
+             print 'expected loss==', np.mean(-1*np.log(net.blobs['cls_prob'].data[:, target_idx]))
         print 'learning rate==', lr
         attack_try = attack_try + 1
 
