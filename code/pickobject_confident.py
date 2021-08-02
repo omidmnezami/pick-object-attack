2e47d877c3930b042e5cc736e07e4b73b6569977
akshay107 <thegphenomenon@gmail.com>
2021-08-02 13:12:34 +0530

    Update pickobject_confident.py

--- code/pickobject_confident.py
+++ code/pickobject_confident.py
@@ -282,6 +282,11 @@ if __name__ == '__main__':
              adversarial_x = np.clip(adversarial_x + grad, clip_min, clip_max)
          
         adversarial_x = np.clip(adversarial_x, 0.0, 255.0)
+	print 'loss====== ', net.blobs['loss_cls'].data
+	if targeted:
+             print 'expected loss==', np.mean(-1*np.log(net.blobs['cls_prob'].data[:, new_class]))
+	else:
+             print 'expected loss==', np.mean(-1*np.log(net.blobs['cls_prob'].data[:, target_idx]))
         attack_try = attack_try + 1
 
     t1 = time.time()
